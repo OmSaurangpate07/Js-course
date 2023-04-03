5263403b0acc89755b7b7151c5c10e78c1da61cd
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-03 13:17:30 +0000

    Functions in JavaScript

---
+++ 02_intermediate/01functions.js
@@ -0,0 +1,25 @@
+// JavaScript Functions
+
+function sayHello(name) {
+    console.log("Hello There, Hitesh!");
+    // console.log("Hello there,", name);
+    console.log(`Hello There, ${name}!`);
+}
+
+// Calling a function
+// sayHello("Om");
+// sayHello("Jayu");
+
+// Referencing a function :
+// sayHello;
+
+function namastey() {
+    return "Hello in India"
+}
+
+namastey();
+
+var greetings = namastey();
+console.log(greetings);
+// OR
+console.log(namastey());
