875a0f43bb0e82ad6319c1fbef3b0928c66badbf
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-03 12:58:37 +0000

    Example of Variables

---
+++ 01_basic/03userLogin.js
@@ -0,0 +1,25 @@
+// Variables:
+// Example: User information form
+
+const uid = "abc123";
+var fullName = "Om Saurangpate";
+var email = "om@dev.com";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 1;
+var isLoggedInWithGoogle = false;
+var isLoggedInWithFacebook = false;
+
+// We can take input from user by using...
+// fullName = prompt("Enter your Name :");
+
+// console.log("uid : " + uid);
+// console.log("Full Name : " + fullName);
+// console.log("Email : " + email);
+
+console.log(`
+Unique ID       : ${uid},
+Name            : ${fullName},
+Email ID        : ${email},
+Course Count    : ${courseCount}
+`);
