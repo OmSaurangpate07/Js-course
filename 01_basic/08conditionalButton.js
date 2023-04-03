83dcce8cea77f7b21607184e02283e3a20453406
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-03 13:09:45 +0000

    Conditional Signout Button

---
+++ 01_basic/08conditionalButton.js
@@ -0,0 +1,14 @@
+// Show signout button if user is authenticated,
+// Otherwise show option to login/signup
+
+var authenticated = true;
+
+// if (authenticated) {
+//     console.log("Signout");
+// } else {
+//     console.log("Login/Signup");
+// }
+
+
+// Ternary operator
+authenticated ? console.log("Signout") : console.log("Login/Signup");
