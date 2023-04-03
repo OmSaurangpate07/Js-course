f98a8a5f1781ade20eee7d6c0c2630d6996e9e1f
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-03 13:13:11 +0000

    Truthy and Falsy Values

---
+++ 01_basic/11truthyAndFalsy.js
@@ -0,0 +1,18 @@
+// Falsy values : 
+// undefined
+// null
+// NaN
+// 0
+// ''
+
+// Type coersion and Type conversion
+// Example :
+
+const value1 = "2";
+const value2 = 2;
+
+// let sum = value1 + value2;
+// console.log(sum);
+
+let sum = Number(value1) + value2;
+console.log(sum);
