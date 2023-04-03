74a8d10e8d9e68882f75b9c4c2ceab15bc2f6455
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-03 12:57:29 +0000

    Variables in JavaScript

---
+++ 01_basic/02variables.js
@@ -0,0 +1,20 @@
+// Variables :
+
+var fullName = "Om Saurangpate";
+
+// How to name variables
+
+// fullName - camelCase
+// full-name
+// full_name - snake_case
+// FullName
+
+var courseName = "React Js Bootcamp";
+var isLoggedIn = true; // false
+var loggedCount = 14;
+console.log(isLoggedIn);
+console.log("isLoggedIn");
+
+var paymentMode;
+paymentMode = "Credit Card";
+console.log(paymentMode);
