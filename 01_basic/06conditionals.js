3dc5bef55572b71852e7b963d916460868e26711
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-03 13:07:02 +0000

    Conditional Statements in JavaScript

---
+++ 01_basic/06conditionals.js
@@ -0,0 +1,20 @@
+// Conditional Statements:
+
+// Syntax:
+/* if (){
+    // Code
+} elseif () {
+    // Code
+} else {
+    // Code
+} */
+
+var temperature = 42;
+
+if (temperature == 20 || temperature < 20) {
+    console.log("Its very cold outside!");
+} else if (temperature > 20 && temperature < 30) {
+    console.log("Its moderate outside!");
+} else {
+    console.log("Its very hot outside!");
+}
