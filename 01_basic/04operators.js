db4ed2cacbec714f9060401fe76db04319d9424e
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-03 12:59:28 +0000

    Operators in JavaScript

---
+++ 01_basic/04operators.js
@@ -0,0 +1,11 @@
+// Operators:
+
+var num1 = 7;
+var num2 = 5;
+
+// Arithmatic Operators(+, -, *, /)
+console.log(num1 + num2);
+
+// Comparison Operator (>, <, <=, >=, ==, !=, ===, !==)
+var ans = num1 > num2;
+console.log(ans);
