05d52a5369832b849f0d10c5fa04f123978435bb
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-04 18:52:23 +0000

    Hoisting in JavaScript

---
+++ 02_intermediate/04hoisting.js
@@ -0,0 +1,18 @@
+// Function declaration is scanned and made avilable
+
+tipper("5")
+function tipper(a) {
+    var bill = parseInt(a);
+    console.log(bill + 5);
+}
+
+// Variable declaration is scanned and made undefined
+var bigTipper = function (a) {
+    bill = parseInt(a);
+    console.log(bill + 15);
+}
+bigTipper("200");
+
+// The below code gives undefined because of the above statement
+console.log(name);
+var name = "Hitesh";
