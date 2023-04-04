002648e29f93fa2a334eaa8e035f35b9ff0c5290
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-04 18:29:44 +0000

    Context in JavaScript

---
+++ 02_intermediate/03jsContext.js
@@ -0,0 +1,22 @@
+// Context in JavaScript
+
+/*
+sayHello();
+
+function sayHello() {
+    console.log("Hello");
+}
+*/
+
+// The below if condition is true according to javaScript
+
+/*
+if (2 == "2") {
+    console.log("This is true");
+}
+*/
+
+// The below if condition is false
+if (2 === "2") {
+    console.log("This is true");
+}
