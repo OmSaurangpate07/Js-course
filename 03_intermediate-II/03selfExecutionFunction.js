60da86ea20b6d6816cd04c5b1e7f01d9e4436427
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-12 12:07:03 +0000

    Self Execution Anonymous Function

---
+++ 03_intermediate-II/03selfExecutionFunction.js
@@ -0,0 +1,14 @@
+// Self Executing Anonymous Function
+
+// function sayHello() {
+//     console.log("I say Hello");
+//     console.log("I again say Hello");
+// }
+// sayHello();
+
+
+// This is the syntax of self executing anonymous function.
+(function() {
+    console.log("I say Hello");
+    console.log("I again say Hello");
+})();
