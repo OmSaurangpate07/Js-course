39b35e5dc7419897f673981dd9604d270e4b5a9d
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-13 06:50:52 +0000

    Closure in JavaScript

---
+++ 03_intermediate-II/05closure.js
@@ -0,0 +1,24 @@
+// Lexical Scoping 
+
+// function init() {
+//     var firstName = "Hitesh";
+//     console.log("I'm init");
+//     function sayFirstName() {
+//         console.log(firstName);
+//     }
+//     return sayFirstName;
+// }
+
+// var value = init();
+// value();
+
+function doAddition(x) {
+    return function(y) {
+        return x + y;
+    }
+}
+
+var add5 = doAddition(5);
+console.log(add5(7));
+
+console.log(doAddition(5)(3));
