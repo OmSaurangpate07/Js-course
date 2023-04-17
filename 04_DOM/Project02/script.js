5a60dc834c876a893b6e130751141d5bbaed66f0
OmSaurangpate07 <129660121+OmSaurangpate07@users.noreply.github.com>
2023-04-17 09:01:04 +0000

    Document Object Model

--- 04_DOM/Project02/script.js
+++ 04_DOM/Project02/script.js
@@ -5,3 +5,19 @@ var orange = document.querySelector(".orange");
 var pink = document.querySelector(".pink");
 
 var center = document.querySelector(".center");
+
+var getBGColor = (colorName) => {
+    return window.getComputedStyle(colorName).backgroundColor;
+}
+
+var magicColorChanger = (element, color) => {
+    return element.addEventListener("click", () => {
+        center.style.background = color'
+    })
+}
+
+magicColorChanger(red, getBGColor(red));
+magicColorChanger(cyan, getBGColor(cyan));
+magicColorChanger(pink, getBGColor(pink));
+magicColorChanger(orange, getBGColor(orange));
+magicColorChanger(violet, getBGColor(violet));
